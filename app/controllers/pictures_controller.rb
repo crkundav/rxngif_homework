dbcba1be7971f51b907b1a75b18d227f062b18b0
crkundav <ckundavaram2014@kellogg.northwestern.edu>
2013-11-03 11:42:01 -0600

    hw 6 Changes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,49 @@
 class PicturesController < ApplicationController
+  
   def show
+  	identifer = params[:id]
+  	delete_success = params[:delete_success]
+  	@return = Picture.find(identifer)
   end
+
+  def index
+  	@return = Picture.all
+  end
+
+  def destroy
+  	id = params[:id]
+  	p = Picture.find(id)
+  	p.delete
+  	flash[:notice] = 'Delete Success'
+  end
+
+  def new
+
+
+  end
+
+  def edit
+   @return = Picture.find(params[:id])
+  end
+
+  def update
+    id = params[:id]
+    name = params[:caption]
+    url = params[:source]
+    p = Picture.find(id)
+    p.caption = name
+    p.source = url
+    p.save
+  end
+
+  def create
+    name = params[:caption]
+    url = params[:source]
+    p = Picture.new
+    p.caption = name
+    p.source = url
+    p.save
+    flash[:notice] = 'Row Added succesfully'
+  end
+  
 end
